a6d20735f9d392ce3d929ac3a341ee6ef7d07e23
Dairo <garcianaranjodairo@gmail.com>
2022-11-22 18:55:59 -0500

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Container, createTheme, ThemeProvider } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,3 +1,104 @@
-const Login = () => <h1>Login</h1>;
+import {
+  Avatar,
+  Box,
+  Button,
+  Checkbox,
+  CssBaseline,
+  FormControlLabel,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Swal from "sweetalert2";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [input, setInput] = useState({
+    user: "",
+    password: "",
+  });
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    if (input.user && input.password) {
+      setInput({ user: "", password: "" });
+      navigate("/admin");
+    } else {
+      Swal.fire("Error!", "Rellena los campos!", "error");
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  return (
+    <div>
+      <CssBaseline />;
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Moto-Wash
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={handleOnSubmit}
+          noValidate
+          sx={{ mt: 1 }}
+        >
+          <TextField
+            margin="normal"
+            type="text"
+            required
+            fullWidth
+            id="user"
+            label="Usuario"
+            name="user"
+            autoFocus
+            value={input.user}
+            onChange={handleOnChange}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Contraseña"
+            type="password"
+            id="password"
+            value={input.password}
+            onChange={handleOnChange}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Recordarme"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Entrar
+          </Button>
+        </Box>
+      </Box>
+    </div>
+  );
+};
 
 export default Login;
